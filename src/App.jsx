a8712c232eb6415a4b58c0faa731cf49bee18d8e
Raihan-143 <raihan172hasan@gmail.com>
2025-06-20 20:40:59 +0600

    initially created parcel pulse client setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-center'>Parcel<span className='text-amber-600'>Pulse</span></h1>
+      
+    </>
+  )
+}
+
+export default App
